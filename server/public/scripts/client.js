e911d53608ca61fd84413781a01ad9d5b1f32859
collinradichel <radichel.collin@gmail.com>
2021-01-10 03:19:32 -0600

    built the POST and PUT routes, added a toggle button to mark a task completed

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -1,93 +1,119 @@
-console.log('client loaded');
+console.log("client loaded");
 
-$(document).ready(function() {
-    console.log('jQ loaded');
+$(document).ready(function () {
+  console.log("jQ loaded");
 
-    setupClickListeners();
+  setupClickListeners();
 
-    getTasks();
-
+  getTasks();
 });
 
-
 function setupClickListeners() {
-    // $('#viewTasks').on('click', '.taskComplete', completeTask);
-    // $('#viewTasks').on('click', '.deleteBtn', deleteBtn);
-    $('#addTaskBtn').on('click', function () {
-        console.log('in addButton on click');
+  $("#viewTasks").on("click", ".taskComplete", completeTaskToggle);
+  // $('#viewTasks').on('click', '.deleteBtn', deleteBtn);
+  $("#addTaskBtn").on("click", function () {
+    console.log("in addButton on click");
 
-        let taskToSend = {
-            dueDate : $('#dateIn').val(),
-            task : $('#taskIn').val(),
-            completed : $('#completedIn').val(),
-            notes : $('#notesIn').val()
-        };
+    let taskToSend = {
+      dueDate: $("#dateIn").val(),
+      task: $("#taskIn").val(),
+      completed: $("#completedIn").val(),
+      notes: $("#notesIn").val(),
+    };
 
-        // call addTask with the new object
-        if (
-            taskToSend.dueDate &&
-            taskToSend.task &&
-            taskToSend.completed &&
-            taskToSend.notes
-        ) {
-           addTask(taskToSend);
-        } else {
-            Swal.fire({
-                title: 'This is not the task you want to submit',
-                imageUrl: 'https://media.giphy.com/media/4560Nv2656Gv0Lvp9F/giphy.gif',
-                confirmButtonText: 'This is not the task I want to submit'
-              })
-        }
-    })
+    // call addTask with the new object
+    if (
+      taskToSend.dueDate &&
+      taskToSend.task &&
+      taskToSend.completed &&
+      taskToSend.notes
+    ) {
+      addTask(taskToSend);
+    } else {
+      Swal.fire({
+        title: "This is not the task you want to submit",
+        imageUrl: "https://media.giphy.com/media/4560Nv2656Gv0Lvp9F/giphy.gif",
+        confirmButtonText: "This is not the task I want to submit",
+      });
+    }
+  });
 }
 
-// function completeTask() {
-//     console.log('clicked complete task');
-
-//     let task = $(this).closest('tr').data('task');
-//     console.log(task);
-
-//     $.ajax({
-//         type : 'PUT'
-//         url : 
-//     })
-// }
-
-
-function getTasks(){
-    console.log('in getTasks');
-
-    $('#viewTasks').empty();
+function addTask(newTask) {
+    console.log("in addTask", newTask);
+  
     $.ajax({
-        type : 'GET',
-        url : '/tasks'
+      type: "POST",
+      url: "/tasks",
+      data: newTask,
     }).then(function (response) {
-        renderTasks(response);
+      $("#dateIn").val(""),
+        $("#taskIn").val(""),
+        $("#completedIn").val(""),
+        $("#notesIn").val(""),
+        getTasks();
+    });
+  }
+
+function completeTaskToggle() {
+  console.log("clicked complete task");
+
+  let task = $(this).closest("tr").data("task");
+  console.log(task);
+
+  $.ajax({
+    type: "PUT",
+    url: `/tasks/${task.id}`,
+    data: task,
+  })
+    .then(function (response) {
+      console.log("Updated");
+      getTasks();
     })
+    .catch(function (error) {
+      alert("error updating completed BOOLEAN BOI");
+    });
+}
+
+function getTasks() {
+  console.log("in getTasks");
+
+  $("#viewTasks").empty();
+  $.ajax({
+    type: "GET",
+    url: "/tasks",
+  }).then(function (response) {
+    renderTasks(response);
+  });
 }
 
 
 function renderTasks(tasks) {
-    for (let i = 0; i < tasks.length; i++) {
-        let task = tasks[i];
-        let $tr = $(`<tr class="table-warning"></tr>`);
-        $tr.data('task', task);
-        $tr.append(`<td>${task.dueDate}</td>`);
-        $tr.append(`<td>${task.task}</td>`);
-        $tr.append(`<td>${task.completed}</td>`);
-        $tr.append(`<td>${task.notes}</td>`);
-        if (task.completed === false) {
-            $tr.append(
-                `<td><button class = "taskComplete btn btn-success">Mark Completed</button></td>`
-            )
-        } else {
-            $tr.append(
-                `<td><button class = "taskComplete btn btn-secondary">Mark NOT Complete</button></td>`
-            );
-        }
-        $tr.append(
-            `<td><button class = "deleteBtn btn btn-danger">DELETE</button></td>`
-        );
-        $('#viewTasks').append($tr);
+  for (let i = 0; i < tasks.length; i++) {
+    let task = tasks[i];
+    let $tr = $(`<tr></tr>`);
+    $tr.data("task", task);
+    $tr.append(`<td>${task.dueDate}</td>`);
+    $tr.append(`<td>${task.task}</td>`);
+    $tr.append(`<td>${task.completed}</td>`);
+    $tr.append(`<td>${task.notes}</td>`);
+    if (task.completed === false) {
+        $($tr).addClass("table-warning");
+      $tr.append(
+        `<td><button class = "taskComplete btn btn-primary">Click when your task is completed</button></td>`
+      );
+    } else if (task.completed === true){
+     $($tr).addClass("table-success");
+      $tr.append(
+        `<td><button class = "taskComplete btn btn-secondary">Task Completed! Click to undo</button></td>`
+      );
+      
     }
+    $tr.append(
+      `<td><button class = "deleteBtn btn btn-danger">DELETE</button></td>`
+    );
+    $("#viewTasks").append($tr);
+  }
 }
+
+
--- server/routes/taskRouter.js
+++ server/routes/taskRouter.js
@@ -5,7 +5,7 @@ const pool = require("../modules/pool.js"); // DB CONNECTION
 
 // GET Route
 taskRouter.get("/", (req, res) => {
-  const queryText = `SELECT * FROM "tasks";`;
+  const queryText = `SELECT * FROM "tasks" ORDER BY "dueDate";`;
   pool
     .query(queryText)
     .then((result) => {
@@ -14,6 +14,57 @@ taskRouter.get("/", (req, res) => {
     .catch((error) => {
       console.log(error);
       res.sendStatus(500);
+    });
+});
+
+taskRouter.post("/", (req, res) => {
+  let newTask = req.body;
+  console.log(`adding new task:`, newTask);
+  let queryText = `INSERT INTO "tasks" ("dueDate", "task", "completed", "notes")
+                    VALUES ($1, $2, $3, $4);`;
+  pool
+    .query(queryText, [
+      newTask.dueDate,
+      newTask.task,
+      newTask.completed,
+      newTask.notes,
+    ])
+    .then((result) => {
+      res.sendStatus(201);
+    })
+    .catch((error) => {
+      console.log(`Error adding new task`, error);
+      res.sendStatus(500);
+    });
+});
+
+taskRouter.put("/:id", (req, res) => {
+  let completed = req.body.completed;
+  let id = req.params.id;
+  let queryText;
+
+  console.log(completed);
+
+  if (completed === "false") {
+    queryText = `UPDATE "tasks"
+                  SET "completed" = true
+                  WHERE "id" = $1`;
+  } else if (completed === "true") {
+    queryText = `UPDATE "tasks"
+                  SET "completed" = false
+                  WHERE "id" = $1`;
+  }
+
+  console.log(`Updating Task with ${id}, setting completed to: `, completed);
+
+  pool
+    .query(queryText, [id])
+    .then((result) => {
+      res.sendStatus(200);
+    })
+    .catch((error) => {
+      console.log(error);
+      res.sendStatus(200);
     });
 });
 
